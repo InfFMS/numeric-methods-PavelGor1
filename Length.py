b3bf2ed055129fc9a8ad0838eb85f0833eb9a659
PavelGor1 <116425791+PavelGor1@users.noreply.github.com>
2025-02-16 22:03:39 +0700

    Update Length.py

--- Length.py
+++ Length.py
@@ -11,7 +11,7 @@ dlina = 0
 j =0
 while j <= np.pi:
     dlina += np.sqrt(0.0001**2+(f(x)-f(x-0.0001))**2)
-    j +=0.01
+    j +=0.0001
 plt.axhline(0, color='red', lw=0.5)
 print(f"Длина кривой: {sum(dlina)}")
 plt.title('f(x)')
